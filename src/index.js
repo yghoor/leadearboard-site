e3bac91d00b12c688197276f26c3b485b9dd3827
Yahya Ghoor <ghooryahya@gmail.com>
2022-06-25 14:52:40 +0200

    Create function to get scores and populate page

--- src/index.js
+++ src/index.js
@@ -6,3 +6,17 @@ const addScoreForm = document.getElementById('add-score-form');
 
 const gameId = 'cU6kSZmLm0gdLbJiJL4f';
 
+async function populate(id) {
+  let scores;
+
+  await fetch(
+    `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${id}/scores/`,
+  )
+    .then((response) => response.json())
+    .then((result) => {
+      scores = result.result;
+    });
+
+  populateBoard(scores);
+}
+

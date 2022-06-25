d0c7c57e233db937974d21ead0742f4dde340647
Yahya Ghoor <ghooryahya@gmail.com>
2022-06-25 14:45:12 +0200

    Create game and set id globally

--- src/index.js
+++ src/index.js
@@ -2,3 +2,5 @@ import './styles.css';
 const refreshButton = document.querySelector('.refresh-btn');
 const addScoreForm = document.getElementById('add-score-form');
 
+const gameId = 'cU6kSZmLm0gdLbJiJL4f';
+
